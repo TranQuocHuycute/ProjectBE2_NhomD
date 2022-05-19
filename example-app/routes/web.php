7b4e446ba8b3869c63bb588eb136a3916dca5c6a
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-05-19 12:53:31 +0700

    thêm layout detail-player

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,18 +1,41 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\CompanyController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
+/*									
+|--------------------------------------------------------------------------									
+| Web Routes									
+|--------------------------------------------------------------------------									
 */
-Route::get('/', function(){
-    return view ('home') ;
-});
+
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom')->middleware('per_page');
 Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
 Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+Route::get('user', [CustomAuthController::class, 'user'])->name('user');
+Route::get('user', [CustomAuthController::class, 'userInfor'])->name('userInfor');
+
+ Route::get('/companies', [CompanyController::class, 'getCompany'])->name('per_page')->middleware('per_page');
+// Route::get('/trainers', [TrainerController::class, 'getTrainers'])->name('per_page')->middleware('per_page');
+
+Route::get('/profile', function () {
+    //
+})->middleware('per_page');
+
+Route::fallback(function () {
+    return view('404');
+});
+
+Route::get('/error', function () {
+    return view('error');
+});
+Route::get('foo', function () {
+    return 'Hello World';
+});
+Route::get('/', function () {
+    return view('home');
+});
