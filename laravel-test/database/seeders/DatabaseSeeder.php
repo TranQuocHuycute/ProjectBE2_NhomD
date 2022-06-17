7c1e4533b54eb33581edada1fd3f865162ba4d66
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-06-17 11:01:44 +0700

    seeder

--- laravel-test/database/seeders/DatabaseSeeder.php
+++ laravel-test/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
            
             PlayerSeeder::class,
+            SeasonsSeeder::class,
+            TeamSeeder::class,
         ]);
     }
 }
